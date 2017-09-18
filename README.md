0c27c8f9edc655381023af45280a6bcb3782d8e8
veaba <908662421@qq.com>
2017-09-18 10:51:16 +0800

    update css width add v-model||value default  setting add demo file update readme file

--- README.md
+++ README.md
@@ -8,14 +8,32 @@
 ```npm
 还没制作npm包
 ```
-## component
-```html
-<address-picker :level="3" :type='["unit","address"]' @on-change="getChangeValue"></address-picker>
+
+## v-model 双向绑定
+```vue
+<address-picker v-model="location" :level="3" :type='["unit","address"]' @on-change="getChangeValue" ></address-picker>
 ```
-## event
+
+```js
+data: function() {
+      return {
+        location: {
+          province: "浙江",
+          city: "",
+          district: "",
+        }
+      }
+    },
+```
+## component被引用
+```vue
+<address-picker v-model="location" :level="3" :type='["unit","address"]' @on-change="getChangeValue"></address-picker>
+```
+## event 子组件返回值 
 
 - @on-change return{Object}
 ```js
+
 methods:{
   getChangeValue(location) {
     console.info(location);
@@ -27,7 +45,7 @@ methods:{
 ## props
 
 - level {number}
-```html
+```vue
 	level 分为三级 1 2 3，
 	level 1 则只会出现一个级联
 	level 2 则只会出现两个级联
@@ -35,19 +53,38 @@ methods:{
 	
 ```
 - type {array}
-```html
+```vue
 	:type='["unit","address"]'
 	
 	目前只支持 两个类型库，需要的话，自己在源码里面增加以适应不同的场合
 	如果空数组则default : address
 ```
+- value {Object}(也可以使用v-model进行双向绑定)
+支持value 传入默认值
+```vue
+	<address-picker :value="location" :level="3" :type='["unit","address"]' @on-change="getChangeValue" ></address-picker>
 
-## lib 库
+```
+此时 在js 里面这样赋予默认值
+```js
+data: function() {
+      return {
+        location: {
+          province: "浙江",
+          city: "",
+          district: "",
+        }
+      }
+    },
+```
+
+
+## lib 库 格式
 ```json
 
 module.exports={
-	 { "name": "北京", "city": [{ "name": "北京", "area": ["东城区", "西城区", "崇文区", "宣武区", "朝阳区", "丰台区", "石景山区", "海淀区", "门头沟区", "房山区", "通州区", "顺义区", "昌平区", "大兴区", "平谷区", "怀柔区", "密云县", "延庆县"] }] },
-{ "name": "天津", "city": [{ "name": "天津", "area": ["和平区", "河东区", "河西区", "南开区", "河北区", "红桥区", "塘沽区", "汉沽区", "大港区", "东丽区", "西青区", "津南区", "北辰区", "武清区", "宝坻区", "宁河县", "静海县", "蓟  县"] }] },
+	{ "name": "北京", "city": [{ "name": "北京", "area": ["东城区", "西城区", "崇文区", "宣武区", "朝阳区", "丰台区", "石景山区", "海淀区", "门头沟区", "房山区", "通州区", "顺义区", "昌平区", "大兴区", "平谷区", "怀柔区", "密云县", "延庆县"] }] },
+	{ "name": "天津", "city": [{ "name": "天津", "area": ["和平区", "河东区", "河西区", "南开区", "河北区", "红桥区", "塘沽区", "汉沽区", "大港区", "东丽区", "西青区", "津南区", "北辰区", "武清区", "宝坻区", "宁河县", "静海县", "蓟  县"] }] },
 }
 ```
 
--- src/vue-multi-address.vue
+++ src/vue-multi-address.vue
@@ -1,14 +1,29 @@
-<!-------------------------------
-*@author veaba https://github.com/veaba
-*@time 2017-9-15
--------------------------------->
+<!--
+*@desc 基于iview ui 多功能级联select 地址插件
+*@desc 参考项目1 https://github.com/iview/iview
+*@desc 参考项目2 https://github.com/G-Veigar/vue-address-picker
+*@desc 参考项目3 https://github.com/ant-design/ant-design
+*@desc 依赖clickoutside
+* level  支持三级 1 2 3、默认3级，props输入
+* type  {array} props 传入数组、默认 address ，支持额外的库，如unit，库由开发者额外提供
+-->
+<style scoped lang="scss">
+  .address-picker {
+    &:after {
+      display: block;
+      content: '';
+      width: 0;
+      height: 0;
+      clear: both;
+    }
+  }
+</style>
+
 <template>
   <div class='address-picker'>
 
     <!--省份-->
-    <div class="ivu-select ivu-select-single" :class="{'ivu-select-visible':selectProvinceStatus}" @click.stop="openSelectProvince"
-         v-clickoutside="handleProvinceClose"
-         :style="{width:level==1?'100%':(level==2?'50%':(level==3?'31%':'100%'))}">
+    <div class="ivu-select ivu-select-single" :class="{'ivu-select-visible':selectProvinceStatus}" @click.stop="openSelectProvince" v-clickoutside="handleProvinceClose" :style="{width:level==1?'100%':(level==2?'48%':(level==3?'33%':'100%'))}">
       <div class="ivu-select-selection">
         <span class="ivu-select-placeholder" v-show="!province">请选择</span>
         <span class="ivu-select-selected-value" v-show="province">{{province}}</span>
@@ -17,17 +32,14 @@
       </div>
       <div class="ivu-select-dropdown" v-show="selectProvinceStatus" :style="styles">
         <ul class="ivu-select-dropdown-list">
-          <li v-for="item in lib" class="ivu-select-item" :class="{'ivu-select-item-selected':province==item.name}"
-              @click="selectProvince(item.name)">{{item.name}}
+          <li v-for="item in lib" class="ivu-select-item" :class="{'ivu-select-item-selected':province==item.name}" @click="selectProvince(item.name)">{{item.name}}
           </li>
         </ul>
       </div>
     </div>
 
     <!--市级-->
-    <div class="ivu-select ivu-select-single" :class="{'ivu-select-visible':selectCityStatus}" @click.stop="openSelectCity"
-         v-clickoutside="handleCityClose"
-         :style="{width:level==1?'100%':(level==2?'50%':(level==3?'31%':'100%'))}">
+    <div class="ivu-select ivu-select-single" v-if="level>1" :class="{'ivu-select-visible':selectCityStatus}" @click.stop="openSelectCity" v-clickoutside="handleCityClose" :style="{width:level==1?'100%':(level==2?'48%':(level==3?'33%':'100%'))}">
       <div class="ivu-select-selection">
         <span class="ivu-select-placeholder" v-show="!city">请选择</span>
         <span class="ivu-select-selected-value" v-show="city">{{city}}</span>
@@ -36,17 +48,14 @@
       </div>
       <div class="ivu-select-dropdown" v-show="selectCityStatus" :style="styles">
         <ul class="ivu-select-dropdown-list">
-          <li v-for="item in cityList" class="ivu-select-item" :class="{'ivu-select-item-selected':city==item.name}"
-              @click="selectCity(item.name)">{{item.name}}
+          <li v-for="item in cityList" class="ivu-select-item" :class="{'ivu-select-item-selected':city==item.name}" @click="selectCity(item.name)">{{item.name}}
           </li>
         </ul>
       </div>
     </div>
 
     <!--地级-->
-    <div class="ivu-select ivu-select-single" :class="{'ivu-select-visible':selectDistrictStatus}" @click.stop="openSelectDistrict"
-         v-clickoutside="handleDistrictClose"
-         :style="{width:level==1?'100%':(level==2?'50%':(level==3?'31%':'100%'))}">
+    <div class="ivu-select ivu-select-single" v-if="level>2" :class="{'ivu-select-visible':selectDistrictStatus}" @click.stop="openSelectDistrict" v-clickoutside="handleDistrictClose" :style="{width:level==1?'100%':(level==2?'48%':(level==3?'33%':'100%'))}">
       <div class="ivu-select-selection">
         <span class="ivu-select-placeholder" v-show="!district">请选择</span>
         <span class="ivu-select-selected-value" v-show="district">{{district}}</span>
@@ -55,8 +64,7 @@
       </div>
       <div class="ivu-select-dropdown" v-show="selectDistrictStatus" :style="styles">
         <ul class="ivu-select-dropdown-list">
-          <li v-for="item in districtList" class="ivu-select-item" :class="{'ivu-select-item-selected':district==item}"
-              @click="selectDistrict(item)">{{item}}
+          <li v-for="item in districtList" class="ivu-select-item" :class="{'ivu-select-item-selected':district==item}" @click="selectDistrict(item)">{{item}}
           </li>
         </ul>
       </div>
@@ -65,9 +73,9 @@
 </template>
 
 <script>
-  import clickoutside from './lib/clickoutside.js'; //外点击事件
-  const addressJson = require('./lib/address.js')
-  const unitJson = require('./lib/unit-json.js')
+  import clickoutside from './clickoutside.js'; //外点击事件
+  const addressJson = require('./address.js')
+  const unitJson = require('./unit-json.js')
   const allJson = [] //全部库的组合
 
   export default {
@@ -79,60 +87,73 @@
       return {
         model1: '',
         lib: this.library(),
-        province: '',
-        city: '',
-        district: '',
+        province: (this.location.province?this.location.province:'')||(this.value.province?this.value.province:''),
+        city: (this.location.city?this.location.city:'')||(this.value.city?this.value.city:''),
+        district: (this.location.district?this.location.district:'')||(this.value.district?this.value.district:''),
         selectProvinceStatus: false,
         selectCityStatus: false,
         selectDistrictStatus: false,
-        screenWidth: '100%'   // 这里是给到了一个默认值 （这个很重要）
+        screenWidth: '100%' // 这里是给到了一个默认值 （这个很重要）
       }
     },
     computed: {
       /*
-      * @desc 下拉宽度
-      * */
+       * @desc 下拉宽度
+       * */
       styles() {
         let style = {};
         if (this.screenWidth) style.width = `${this.screenWidth}px`;
         return style;
       },
-      //计算出来省份即一级的列表
-      provinceList: function () {
-
-      },
       //计算出来市级即二级的列表
-      cityList: function () {
+      cityList: function() {
         const thisProvince = this.province
-        let thisArr = this.lib.filter(function (index) {
+        let thisArr = this.lib.filter(function(index) {
           return index.name == thisProvince
         })
         return thisArr[0] ? thisArr[0].city : []
       },
       //计算出来县级即三级的列表
-      districtList: function () {
+      districtList: function() {
         const thisCity = this.city;
-        let thisArr = this.cityList.filter(function (index) {
+        let thisArr = this.cityList.filter(function(index) {
           return index.name == thisCity
         })
         return thisArr[0] ? thisArr[0].area : []
-      }
+      },
 
     },
     /**
      * @desc level 1/2/3 => 省/市/县
      * @desc type 引入的库数组 [unit,address]
      */
-    props: ['opts', 'level', 'type'],
-    created() {
+    props: {
+      value:{
+        default:Object
+      },
+      level: {
+        default: ""
+      },
+      type: {
+        default: Number
+      },
+      location:{
+        default:Object
+      }
+    },
 
+    model: {
+      prop: 'location',
+      event: 'on-change'
+    },
+    created() {
     },
 
     methods: {
       /**
        * @desc 库文件
        * */
-      library: function () {
+      library: function() {
         //1、判断type 字段
         if (this.type) {
           //2、存在且返回index 、否则返回 -1 indexOf 和IE有兼容性问题
@@ -165,13 +186,32 @@
        * */
       selectProvince(_province) {
         this.province = _province
-        this.city='';
-        this.district=''
-        this.$emit('on-change',{
-          province:this.province,
-          city:this.city,
-          district:this.district
-        })
+        this.city = '';
+        this.district = ''
+        if (this.level == 1) {
+          this.$emit('on-change', {
+            province: this.province,
+
+          })
+        } else if (this.level == 2) {
+          this.$emit('on-change', {
+            province: this.province,
+            city: this.city,
+
+          })
+        } else if (this.level == 3) {
+          this.$emit('on-change', {
+            province: this.province,
+            city: this.city,
+            district: this.district
+          })
+        } else {
+          this.$emit('on-change', {
+            province: this.province,
+            city: this.city,
+            district: this.district
+          })
+        }
       },
 
       /**
@@ -179,23 +219,40 @@
        * */
       selectCity(_city) {
         this.city = _city
-        this.district=''
-        this.$emit('on-change',{
-          province:this.province,
-          city:this.city,
-          district:this.district
-        })
+        this.district = ''
+        if (this.level == 2) {
+          this.$emit('on-change', {
+            province: this.province,
+            city: this.city,
+
+          })
+        } else if (this.level == 3) {
+          this.$emit('on-change', {
+            province: this.province,
+            city: this.city,
+            district: this.district
+          })
+        } else {
+          this.$emit('on-change', {
+            province: this.province,
+            city: this.city,
+            district: this.district
+          })
+        }
+
       },
       /**
        * @desc 选择县级
        * */
       selectDistrict(_district) {
         this.district = _district
-        this.$emit('on-change',{
-          province:this.province,
-          city:this.city,
-          district:this.district
+
+        this.$emit('on-change', {
+          province: this.province,
+          city: this.city,
+          district: this.district
         })
+
       },
 
       /**
@@ -220,21 +277,21 @@
       /**
        *@desc 省级触发下拉事件
        */
-      openSelectProvince(){
+      openSelectProvince() {
         this.selectProvinceStatus = !this.selectProvinceStatus
       },
       /**
        * @desc 市级触发下拉事件
        * */
-      openSelectCity(){
-        this.selectCityStatus=!this.selectCityStatus
+      openSelectCity() {
+        this.selectCityStatus = !this.selectCityStatus
       },
 
       /**
        * @desc 县级触发下拉事件
        * */
-      openSelectDistrict(){
-        this.selectDistrictStatus=!this.selectDistrictStatus
+      openSelectDistrict() {
+        this.selectDistrictStatus = !this.selectDistrictStatus
       }
 
     },
@@ -244,13 +301,13 @@
           this.screenWidth = val
           this.timer = true
           let that = this
-          setTimeout(function () {
+          setTimeout(function() {
             that.timer = false
           }, 400)
         }
       }
     },
-    mounted: function () {
+    mounted: function() {
       const _that = this
       window.onresize = () => {
         return (() => {
@@ -266,7 +323,6 @@
 </script>
 
 <style lang="scss" scoped>
-  .address-picker {
-  }
+  .address-picker {}
 </style>
 
