8cf4e6d56f75c0f27123b03f054b558ecb9f69f7
veaba <908662421@qq.com>
2019-06-15 23:55:12 +0800

    fix install path errp

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  *@author veaba https://github.com/veaba
  *@date 2017/9/15
  ***********************/
-var vueMultiAddress  = require('./src/lib/vue-address-picker.vue');
+var vueMultiAddress  = require('./src/vue-address-picker.vue');
 
 module.exports = function install (Vue) {
   Vue.component(vueMultiAddress.name, vueMultiAddress)
